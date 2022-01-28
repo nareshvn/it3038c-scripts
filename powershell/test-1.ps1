736d14bfc0a5fec7bd5bfa2e01aa5edb7e75b8fc
nareshvn <nareshvn@mail.uc.edu>
2022-01-27 20:27:09 -0500

    Updated Powershell files

--- powershell/test-1.ps1
+++ powershell/test-1.ps1
@@ -14,4 +14,7 @@ $myver = " Version ",$host.version.Major, " Today's date is "
 $myDate = get-date -Format "dddd, MM, dd, yyyy" 
 $myHname = " Hostname is ", $env:COMPUTERNAME
 Write-Host " This machine's IP address is ", $myip,  $user, $myHname , $mypname,  $myver,  $myDate
-Add-Content -Path "C:\it3038c-scripts\powershell\test1.txt" -Nonewline -Value " This machine's IP address is ",$myip, $user,$myHname,$mypname,$myver,$myDate
+$myop=" This machine's IP address is ", $myip,  $user, $myHname , $mypname,  $myver,  $myDate
+#Add-Content -Path "C:\it3038c-scripts\powershell\test1.txt" -Nonewline -Value " This machine's IP address is ",$myip, $user,$myHname,$mypname,$myver,$myDate
+Add-Content -Path "C:\it3038c-scripts\powershell\test1.txt" -Nonewline -Value $myop
+Send-MailMessage -Cc "leonardf@ucmail.uc.edu" -To "nareshvn@mail.uc.edu" -From "venkaatnaresh@gmail.com" -Subject "IT3038C Windows SysInfo" -Body "Lab 3 output file attached" -Attachments .\test1.txt -SmtpServer 'smtp.gmail.com' -port 587 -UseSSL -Credential (Get-Credential)
