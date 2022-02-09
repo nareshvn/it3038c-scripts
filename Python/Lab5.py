d4f9296883cdd22d3d37d55ab14c7c360ad4d14f
nareshvn <nareshvn@mail.uc.edu>
2022-02-08 20:21:17 -0500

    added Lab5.py

---
+++ Python/Lab5.py
@@ -0,0 +1,12 @@
+print("counting vowels and consonants in a string...");
+mystr=input("Enter a string: ");
+vow=0
+cons=0
+for i in mystr:
+	if (i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or
+i=='o' or i=='U' or i=='I'):
+		vow=vow+1;
+	else:
+		cons=cons+1;
+print('vowels counted: ',vow);
+print('consonants counted: ',cons);
