1a8874aa902ff6092ba3b02bd403a4df2ef6e9b8
nareshvn <nareshvn@mail.uc.edu>
2022-02-18 18:04:06 -0500

    Project 1 scripts

--- Project1/NumberGame.py
+++ Project1/NumberGame.py
@@ -7,12 +7,12 @@ num=int(input("Guess my number between 0 and 100: "));
 mynum=randint(0,100);
 print ("My number : ",mynum);
 if (num < 0 or num >100):
-print(" you didnt follow my instructions ");
+    print(" you didnt follow my instructions ");
 else:
-if (num==mynum):
-print("Guessed correctly ",num);
-else:
-if (mynum < num):
-print("My random number ",mynum," is lesser than",num);
-else:
-print("My random number ",mynum, " is greater than",num);
+    if (num==mynum):
+        print("Guessed correctly ",num);
+    else:
+        if (mynum < num):
+            print("My random number ",mynum," is lesser than",num);
+        else:
+            print("My random number ",mynum, " is greater than",num);
--- Project1/PrimeNumber.py
+++ Project1/PrimeNumber.py
@@ -5,16 +5,17 @@ Flag=False
 nonPrime="";
 
 if (num==0 or num > 0):
-print(0);
+    print(0);
 if (num==1 or num > 1):
-print(1);
+    print(1);
 for i in range(2, num+1):
-for j in range(2, i):
-if (i % j) == 0:
+    for j in range(2, i):
+        if (i % j) == 0:
 # print("i = ",i, " j = ", j, ";",i,"%",j," is ", i%j );
-Flag=True
-break
-else:
-Flag=False
-if (Flag == False):
-print(i)
+            Flag=True
+            break
+        else:
+            Flag=False
+
+    if (Flag == False):
+        print(i);
