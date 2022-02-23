f45ef7c05686f8ade3a0a8dab85343cb60c29252
nareshvn <nareshvn@mail.uc.edu>
2022-02-23 18:14:16 -0500

    Week 7 Lab

---
+++ powershell/README.md
@@ -0,0 +1,10 @@
+# README
+## User_info.ps1 gets the LocalUser, ComputerInfo and the User's Clipboard information
+### Info: UserName: Administrator 
+### Info: UserName: CECHUser 
+### Info: UserName: DefaultAccount 
+### Info: UserName: Guest 
+### Info: UserName: WDAGUtilityAccount 
+### UserName Info: @{CsUserName=UC-NARESHVN\Administrator}
+### Logon Server Info: @{LogonServer=\\UC-NARESHVN}
+### User lately copied info in clipboard: C:\it3038c-scripts\powershell
---
+++ powershell/user_info.ps1
@@ -0,0 +1,27 @@
+﻿$Machines = 'localhost'
+$myinfo='' 
+$Logfile='C:\logs\user_info.txt'
+
+$pt=Get-LocalUser
+
+    $sample = 1 
+
+    foreach ($p in $pt) { 
+          $date = Get-Date -Format g 
+
+        $myinfo= "UserName: {0} " -f $p
+        Add-Content $Logfile "Info: ${myinfo}"
+
+        $sample++ 
+      
+    } 
+   $myinfo = Get-ComputerInfo -Property "CSUserName"
+   Add-Content $Logfile "UserName Info: ${myinfo}"
+    $myinfo = Get-ComputerInfo -Property "LogonServer"
+   Add-Content $Logfile "Logon Server Info: ${myinfo}"
+    $myinfo = Get-ClipBoard
+    Add-Content $Logfile "User lately copied info in clipboard: ${myinfo}"
+
+
+    
+  
